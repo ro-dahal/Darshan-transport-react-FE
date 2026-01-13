2d6d92806d54ad9f302d3b9dc0580f187449b67f
Subin-Gurung49 <grgsubn@gmail.com>
2026-01-13 19:23:53 +0545

    style: format code for consistency in CI/CD workflow and MainLayout component

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Navbar } from '../../layout/components/Navbar';
-import { Preloader } from '../../layout/components/Preloader';
-import { ScrollToTop } from '../../layout/components/Scroll/ScrollToTop';
-import { Footer } from '../../layout/components/Footer';
-import WhatsAppFloat from '../../features/marketing/shared/components/WhatsAppFloat';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../../layout/components/Navbar";
+import { Preloader } from "../../layout/components/Preloader";
+import { ScrollToTop } from "../../layout/components/Scroll/ScrollToTop";
+import { Footer } from "../../layout/components/Footer";
+import WhatsAppFloat from "../../features/marketing/shared/components/WhatsAppFloat";
 
 export const MainLayout: React.FC = () => (
   <>
@@ -13,9 +13,9 @@ export const MainLayout: React.FC = () => (
     <Navbar />
     <Outlet />
     <Footer />
-    <WhatsAppFloat 
-      phoneNumber="+9779847688471" 
-      message="Hello! I'm interested in your transport services." 
+    <WhatsAppFloat
+      phoneNumber="+9779847688471"
+      message="Hello! I'm interested in your transport services."
     />
   </>
 );
